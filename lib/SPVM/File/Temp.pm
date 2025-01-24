226e045adc00f8c0e2173f4400467d2cadf4b188
Yuki Kimoto <kimoto.yuki@gmail.com>
2025-01-25 08:57:10 +0900

    Call Fn#destroy_runtime_permanent_vars method in the end of tests. and etc.
    0.023 2025-01-24
      [Requirement Changes]
        * Require SPVM 0.990042.
      [Test Improvement]
        * Call Fn#destroy_runtime_permanent_vars method in the end of tests.

--- lib/SPVM/File/Temp.pm
+++ lib/SPVM/File/Temp.pm
@@ -10,7 +10,7 @@ SPVM::File::Temp - Creating Temporary Files and Directories
 
 =head1 Description
 
-The File::Temp class in L<SPVM> has methods to create temporary files and directories.
+File::Temp class in L<SPVM> has methods to create temporary files and directories.
 
 =head1 Usage
 
