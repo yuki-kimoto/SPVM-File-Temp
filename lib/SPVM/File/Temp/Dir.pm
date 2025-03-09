e3f94c8d893c24751b88530b83deef34d5cbad97
Yuki Kimoto <kimoto.yuki@gmail.com>
2025-03-10 07:50:04 +0900

    Add Dir option logic.

--- lib/SPVM/File/Temp/Dir.pm
+++ lib/SPVM/File/Temp/Dir.pm
@@ -24,6 +24,38 @@ C<has dirname : ro string;>
 
 Returns the directory name.
 
+=head2 new
+
+C<static method new : L<File::Temp::Dir|SPVM::File::Temp::Dir> ($options : object[] = undef);>
+
+Creates a new L<File::Temp::Dir|SPVM::File::Temp::Dir> object given the options $options, and returns it.
+
+=head3 new Options
+
+=head4 DIR option
+
+C<DIR>
+
+Type:string, Default:undef
+
+=head4 TMPDIR option
+
+C<TMPDIR>
+
+Type:L<Int|SPVM::Int>, Default:0
+
+=head4 TEMPLATE option
+
+C<TEMPLATE>
+
+Type:string, Default:undef
+
+=head4 UNLINK option
+
+C<UNLINK>
+
+Type:L<Int|SPVM::Int>, Default:1
+
 =head1 Copyright & License
 
 Copyright (c) 2023 Yuki Kimoto
